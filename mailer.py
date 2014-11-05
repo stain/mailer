a1437bcb430b260d84a9994572df2ccce9a72cb4
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-11-05 14:41:09 +0000

    configuration based smtp config

--- mailer.py
+++ mailer.py
@@ -1,14 +1,63 @@
 #!/usr/bin/env python2.5
 
+from ConfigParser import ConfigParser
 import email
 import email.utils
-import time
+import getpass
+import os
 import re
-
 import smtplib
-smtp = smtplib.SMTP("smtp.gmail.com", 587) 
-smtp.starttls()
-smtp.login("stian@mygrid.org.uk", "*********")
+import sys
+import time
+
+DUMMY_USER="nobody@gmail.com"
+
+def _configpath():
+    configpath = os.path.expanduser("~/.mailer")
+    if not os.path.exists(configpath):
+        open(configpath, "w").close()
+        os.chmod(configpath, 0600)
+    return configpath
+
+_config = None
+def config():
+    global _config
+    if _config is not None:
+        return _config
+    _config = ConfigParser()
+    _config.read(_configpath())
+    if not _config.has_section("smtp"):
+        template_config()
+        save_config()
+    return _config
+
+def save_config():
+    fp = open(_configpath(), "w")
+    try:
+        config().write(fp)
+    finally:
+        fp.close()
+
+def template_config():
+    if not config().has_section("smtp"):
+        config().add_section("smtp")
+    config().set("smtp", "host", "smtp.gmail.com")    
+    config().set("smtp", "port", 587)    
+    config().set("smtp", "tls", True)
+    config().set("smtp", "username", DUMMY_USER)
+    config().set("smtp", "password", "WrongPassword")
+
+if config().get("smtp", "username") == DUMMY_USER:
+    print >>sys.stderr, "You need to configure smtp settings in", _configpath()
+    sys.exit(1)
+
+
+smtp = smtplib.SMTP(config().get("smtp", "host"), config().getint("smtp", "port"))
+
+if (config().getboolean("smtp", "tls")):
+    smtp.starttls()
+if (config().has_option("smtp", "username")):    
+    smtp.login(config().get("smtp", "username"), config().get("smtp", "password"))
 
 
 def send_email(to, counter):
